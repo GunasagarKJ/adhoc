fe0049cca2f2aa52bd940cf01365621ee1ee07a0
Gunasagar K J <Gunasagar K J@DESKTOP-0V9474S>
2020-10-13 21:04:46 -0700

    branch 1

--- AngleOnClock/angleOnClock/AngleOnClock.java
+++ AngleOnClock/angleOnClock/AngleOnClock.java
@@ -7,62 +7,57 @@ public class AngleOnClock {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-		Scanner sc= new Scanner(System.in);
-		try{
-		Time t= new Time(sc.nextInt(),sc.nextInt());
-		}
-		catch(InputMismatchException e){
+
+		Scanner sc = new Scanner(System.in);
+		try {
+			Time t = new Time(sc.nextInt(), sc.nextInt());
+			System.out.println(t.getTime());
+		} catch (InputMismatchException e) {
 			System.out.println("Enter an integer");
+			e.printStackTrace();
+
 		}
-		
-		
-		
+
 	}
 
 }
 
-class Time{
+class Time {
 	private int hr;
 	private int min;
-	
-	public Time(int a,int b){
-		hr=a;
-		min=b;
-	}	
-	
-	public int getValueHr(){
-		return hr;
-	}
-	
-	public int getValueMin(){
-		return min;
-	}
-	
-	public void assignTime(int a, int b){
-		hr=a;
-		min=b;
+
+	public Time(int a, int b) {
+		hr = a;
+		min = b;
 	}
 
-	public void assignTime(int a){
-		hr=a;
-		min=0;
+	public int getValueHr() {
+		return hr;
 	}
-	
-	public int getTime(){
-		
-		return Math.abs(this.hr*5*6+this.min/12*6-this.min*6);
-		
+
+	public int getValueMin() {
+		return min;
+	}
+
+	public void assignTime(int a, int b) {
+		hr = a;
+		min = b;
+	}
+
+	public void assignTime(int a) {
+		hr = a;
+		min = 0;
+	}
+
+	public int getTime() {
+
+		return Math.abs(this.hr * 5 * 6 + this.min / 12 * 6 - this.min * 6);
+
 		/*
-		 *60-->360
-		 *1-->6
-		 *5-->60
-		 *1-->12 
-		 * 
-		 * 
+		 * 60-->360 1-->6 5-->60 1-->12
 		 *
 		 */
-		
+
 	}
 
 }
